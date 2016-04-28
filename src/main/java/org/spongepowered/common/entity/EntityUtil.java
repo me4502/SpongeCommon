f3a331399c95a2ddcbbaead715a6d72b9e839a13
Gabriel Harris-Rouquette <gabizou@me.com>
2016-04-28 16:57:09 -0700

    Expand implementation of Archetypes a bit more.
    Signed-off-by: Gabriel Harris-Rouquette <gabizou@me.com>

--- src/main/java/org/spongepowered/common/entity/EntityUtil.java
+++ src/main/java/org/spongepowered/common/entity/EntityUtil.java
@@ -57,6 +57,8 @@ import net.minecraft.world.WorldProviderEnd;
 import net.minecraft.world.WorldProviderSurface;
 import net.minecraft.world.WorldServer;
 import org.spongepowered.api.data.type.Profession;
+import org.spongepowered.api.entity.Transform;
+import org.spongepowered.api.entity.EntitySnapshot;
 import org.spongepowered.api.entity.living.Living;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.event.entity.MoveEntityEvent;
@@ -430,6 +432,9 @@ public final class EntityUtil {
             throw new IllegalArgumentException("Not a native SpongeAPI entity!");
         }
         return (org.spongepowered.api.entity.Entity) mixinEntity;
+    }
+    public static EntitySnapshot createSnapshot(Entity entity) {
+        return fromNative(entity).createSnapshot();
     }
 
     public static boolean changeWorld(net.minecraft.entity.Entity entity, Location<org.spongepowered.api.world.World> location, int currentDim, int targetDim) {
